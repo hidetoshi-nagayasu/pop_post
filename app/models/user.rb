e5726361d0f48b90ea543b057b50b0493f663c2f
Hidetoshi Nagayasu <hidetoshi.nagayasu@gmail.com>
2023-07-27 23:33:14 +0900

    Remove password confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 50 }
 end
