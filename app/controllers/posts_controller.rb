76df46ecd23e40b912743e9152f71343c677a7ff
Hidetoshi Nagayasu <hidetoshi.nagayasu@gmail.com>
2023-08-03 00:17:01 +0900

    Allow to create post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,59 @@
 class PostsController < ApplicationController
+  before_action :require_login, except: %i[ index ]
+  before_action :set_post, only: %i[ show edit update destroy ]
+
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params.merge(user_id: current_user.id))
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to posts_path, notice: "投稿しました！" }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
   end
 
   def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "投稿を更新しました！" }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "投稿を削除しました！" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
